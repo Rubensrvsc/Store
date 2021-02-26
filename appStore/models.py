f35bcd7b2ad0e07b103c04710d1b7db7e1040b64
rubensrvsc <Rubensspfc100@gmail.com>
2021-02-26 09:24:48 -0300

    build: add models and migrations with your relationships

--- appStore/models.py
+++ appStore/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductType(models.Model):
+    name_product_type = models.CharField(max_length=30)
+
+class Size(models.Model):
+    size = models.CharField(max_length=30)
+    
+class ProductCategory(models.Model):
+    name_product_category = models.CharField(max_length=30)
+    
+class Product(models.Model):
+    name_product = models.CharField(max_length=100)
+    color = models.CharField(max_length=30)
+    price = models.FloatField()
+    product_category = models.ForeignKey(ProductCategory, related_name="category_product",on_delete=models.CASCADE)
+    product_size = models.ForeignKey(Size, related_name="size_product",on_delete=models.CASCADE)
+    product_type = models.ForeignKey(ProductType, related_name="type_product",on_delete=models.CASCADE)
+    
