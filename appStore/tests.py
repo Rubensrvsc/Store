a9fcd5301cdebd994be4533aaa982e03cf9c5f25
rubensrvsc <Rubensspfc100@gmail.com>
2021-03-03 10:01:15 -0300

    test: add structure of tests

--- appStore/tests.py
+++ appStore/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from rest_framework.test import APIRequestFactory, APIClient
+from .models import *
+from rest_framework import status
 # Create your tests here.
+
+class ProductTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        pass
