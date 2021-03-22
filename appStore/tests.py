818574813e5805902e239fb7ef2e0c34ff6059b1
rubensrvsc <Rubensspfc100@gmail.com>
2021-03-22 16:21:49 -0300

    test: add create test for product_category

--- appStore/tests.py
+++ appStore/tests.py
@@ -4,8 +4,16 @@ from .models import *
 from rest_framework import status
 # Create your tests here.
 
-class ProductTest(TestCase):
+class ProductCategoryTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        pass
+        ProductCategory.objects.create(name_product_category="Infantil")
+        ProductCategory.objects.create(name_product_category="Adulto")
+        ProductCategory.objects.create(name_product_category="Idoso")
+
+    def test_create_product_category(self):
+        factory = APIClient()
+        product_category = {"name_product_category": "infantil"}
+        request = factory.post('/createcategory',product_category)
+        self.assertEquals(request.status_code,status.HTTP_201_CREATED)
