82a54a1776d3e1c0cf5ad42dbc360cbb2cc6259f
rubensrvsc <Rubensspfc100@gmail.com>
2021-03-22 22:03:31 -0300

    test: add test of crete product

--- appStore/tests.py
+++ appStore/tests.py
@@ -12,6 +12,27 @@ class ProductCategoryTest(TestCase):
         ProductCategory.objects.create(name_product_category="Adulto")
         ProductCategory.objects.create(name_product_category="Idoso")
 
+        
+        ProductType.objects.create(name_product_type="camisa")
+        ProductType.objects.create(name_product_type="calça")
+        
+        Size.objects.create(size="GG")
+        Size.objects.create(size="G")
+        
+        size = Size.objects.get(id=1)
+        category_product = ProductCategory.objects.get(id=1)
+        type_product = ProductType.objects.get(id=1)
+
+        Product.objects.create(
+            name_product="camisa 1",
+            price=100,
+            color="red",
+            product_category = category_product,
+            product_size = size,
+            product_type = type_product
+        )
+
+
     def test_create_product_category(self):
         factory = APIClient()
         product_category = {"name_product_category": "infantil"}
@@ -23,3 +44,25 @@ class ProductCategoryTest(TestCase):
         products_categories = ProductCategory.objects.all().count()
         request = factory.get("/productcategories")
         self.assertEquals(products_categories,3)
+
+    def test_create_product(self):
+        factory = APIClient()
+        
+        size = Size.objects.get(id=1)
+        category_product = ProductCategory.objects.get(id=1)
+        type_product = ProductType.objects.get(id=1)
+        
+
+        product = {'name_product':"camisa 1",
+            'price':100,
+            'color':"red",
+            'product_category' : category_product.id,
+            'size': size.id,
+            'product_type': type_product.id
+            }
+        response = factory.post('/create_product',product)
+        self.assertEquals(response.status_code,status.HTTP_301_MOVED_PERMANENTLY)
+
+
+    
+
