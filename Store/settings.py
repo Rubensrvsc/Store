03cd12ff0e4047d17e28758f418870f0d5d2c2bb
rubensrvsc <Rubensspfc100@gmail.com>
2021-03-23 19:04:37 -0300

    refactor: pulling out django filter in settings

--- Store/settings.py
+++ Store/settings.py
@@ -74,9 +74,8 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
-}
+    'PAGE_SIZE': 3
+    }
 
 WSGI_APPLICATION = 'Store.wsgi.application'
 
