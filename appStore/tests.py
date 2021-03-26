37961d42696beb9ee803060cce1e46f9f6b05bf5
rubensrvsc <Rubensspfc100@gmail.com>
2021-03-26 14:42:24 -0300

    test: add test for search between prices

--- appStore/tests.py
+++ appStore/tests.py
@@ -23,10 +23,41 @@ class ProductCategoryTest(TestCase):
         category_product = ProductCategory.objects.get(id=1)
         type_product = ProductType.objects.get(id=1)
 
+        size_two = Size.objects.get(id=2)
+        category_product_two = ProductCategory.objects.get(id=2)
+        type_product_two = ProductType.objects.get(id=2)
+
         Product.objects.create(
             name_product="camisa 1",
             price=100,
             color="red",
+            product_category = category_product,
+            product_size = size,
+            product_type = type_product
+        )
+
+        Product.objects.create(
+            name_product="camisa 2",
+            price=150,
+            color="green",
+            product_category = category_product_two,
+            product_size = size_two,
+            product_type = type_product_two
+        )
+
+        Product.objects.create(
+            name_product="camisa 3",
+            price=190,
+            color="yellow",
+            product_category = category_product_two,
+            product_size = size_two,
+            product_type = type_product_two
+        )
+
+        Product.objects.create(
+            name_product="camisa 4",
+            price=220,
+            color="magento",
             product_category = category_product,
             product_size = size,
             product_type = type_product
@@ -67,6 +98,12 @@ class ProductCategoryTest(TestCase):
 
         factory = APIClient()
         response = factory.get('/products')
+        self.assertEquals(response.status_code,status.HTTP_200_OK)
+
+    def test_search_price_products(self):
+
+        factory = APIClient()
+        response = factory.get('/searchpriceproduct/80/170')
         self.assertEquals(response.status_code,status.HTTP_200_OK)
 
 
